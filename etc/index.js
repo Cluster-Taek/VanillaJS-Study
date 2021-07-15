aa069d83c452ae1c617456842c04d110326e070f
Cluster-Taek <yst811@naver.com>
2021-07-15 23:24:52 +0900

    임시 커밋

---
+++ etc/index.js
@@ -0,0 +1,15 @@
+// DOM(Document Object Module)
+
+/* Memo
+18강부터 시작
+https://www.youtube.com/watch?v=LnuLDGO5848&list=PLLUCyU7SBaR7tOMe-ySJ5Uu1UlEBznxTr&index=18
+*/
+
+
+const title = document.getElementById('title')
+title.innerHTML = "Hi from JS";
+title.style.color = "green";
+document.title = "Wonderful!"
+console.dir(document)
+
+
