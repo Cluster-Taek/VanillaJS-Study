9a2b4196ee87958524ce1720056378856f4d4095
Cluster-Taek <yst811@naver.com>
2021-07-15 23:07:05 +0900

    Calculator.js

---
+++ etc/Calculator.js
@@ -0,0 +1,18 @@
+const calculator = {
+    plus : function(a, b) {
+        return a + b;
+    },
+    minus : function(a, b) {
+        return a - b;
+    },
+    multiple : function(a, b) {
+        return a * b;
+    },
+    divide : function(a, b) {
+        return a / b;
+    }
+}
+
+console.log(calculator.multiple(5, 4))
+
+
