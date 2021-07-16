da573730bfdc71afa1a191f7f87e0e603d9c7bd0
Cluster-Taek <yst811@naver.com>
2021-07-16 14:27:38 +0900

    JavaScript EventListener

--- etc/index.js
+++ etc/index.js
@@ -1,51 +1,27 @@
 // DOM(Document Object Module)
 
-/* Memo
-18강부터 시작
-https://www.youtube.com/watch?v=LnuLDGO5848&list=PLLUCyU7SBaR7tOMe-ySJ5Uu1UlEBznxTr&index=18
-*/
+/** 22부터 시작
+ * https://www.youtube.com/watch?v=f0nBj0YMBUI&list=PLLUCyU7SBaR7tOMe-ySJ5Uu1UlEBznxTr&index=22
+ */
 
-// EventListner 1
-
-function handleResize(event) {
-    // 현재 작동하는 이벤트 출력
-    console.log(event) 
-}
-
-window.addEventListener("resize", handleResize()); // handleResize 함수를 지금 즉시 호출
-window.addEventListener("resize", handleResize); // handleResize 함수를 resize 시 호출
-
-// EventListner 2
+// EventListner
 const title = document.querySelector("#title");
-const BASIC_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "rgb(154, 236, 219)";
+
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    if(title.style.color === BASIC_COLOR) {
-        title.style.color = OTHER_COLOR
-    } else {
-        title.style.color = BASIC_COLOR
-    }
-    console.log(title.style.color)
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if(hasClass) {
+    //     title.classList.remove(CLICKED_CLASS);
+    // }else {
+    //     title.classList.add(CLICKED_CLASS);
+    // } 
+
+    title.classList.toggle(CLICKED_CLASS);
 }
 
-// 초기화 함수
 function init() {
-    title.style.color = BASIC_COLOR;
-    title.addEventListener("mouseenter", handleClick)
+    title.addEventListener("click", handleClick)
 }
 
-init();
-
-// EventListener 3
-
-function handleOffline() {
-    console.log("Offline~")
-}
-
-function handleOnline() {
-    console.log("Online~")
-}
-
-window.addEventListener("online", handleOnline);
-window.addEventListener("offline", handleOffline);
+init()
